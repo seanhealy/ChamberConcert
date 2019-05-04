d42834bc98b99d8c0d42dfcfbc0f67d0095dd6ef
Sean Healy <s@xib.ca>
2019-05-03 22:11:48 -0600

    Ears

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 var analyser = audioCtx.createAnalyser();
 analyser.smoothingTimeConstant = 0;
 
+const numbersView = document.getElementById("numbers");
+const historyView = document.getElementById("history");
+
 if (navigator.mediaDevices.getUserMedia) {
   console.log("getUserMedia supported.");
   var constraints = { audio: true };
@@ -20,8 +23,8 @@ if (navigator.mediaDevices.getUserMedia) {
   console.log("getUserMedia not supported on your browser!");
 }
 
-// analyser.fftSize = 2048;
-analyser.fftSize = 32;
+analyser.fftSize = 2048;
+// analyser.fftSize = 32;
 var bufferLength = analyser.frequencyBinCount;
 var dataArray = new Uint8Array(bufferLength);
 analyser.getByteTimeDomainData(dataArray);
@@ -35,34 +38,13 @@ const HEIGHT = canvas.clientHeight;
 
 // draw an oscilloscope of the current audio source
 
+let recordedCharacter = false;
+let lastIntValue = 0;
+let lastChange = Date.now();
+const timeThreshold = 20;
+
 function draw() {
   requestAnimationFrame(draw);
-
-  // analyser.getByteTimeDomainData(dataArray);
-  // console.log(dataArray[0]);
-
-  // canvasCtx.fillStyle = "rgb(200, 200, 200)";
-  // canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
-
-  // canvasCtx.lineWidth = 2;
-  // canvasCtx.strokeStyle = "rgb(0, 0, 0)";
-
-  // canvasCtx.beginPath();
-
-  // var sliceWidth = (canvas.width * 1.0) / bufferLength;
-  // var x = 0;
-
-  // for (var i = 0; i < bufferLength; i++) {
-  //   var v = dataArray[i] / 128.0;
-  //   var y = (v * canvas.height) / 2;
-
-  //   if (i === 0) {
-  //     canvasCtx.moveTo(x, y);
-  //   } else {
-  //     canvasCtx.lineTo(x, y);
-  //   }
-
-  //   x += sliceWidth;
 
   analyser.getByteFrequencyData(dataArray);
 
@@ -73,16 +55,67 @@ function draw() {
   var barHeight;
   var x = 0;
 
-  // console.log(dataArray[0]);
-
   for (var i = 0; i < bufferLength; i++) {
     barHeight = dataArray[i];
 
-    canvasCtx.fillStyle = "rgb(" + (barHeight + 100) + ",50,50)";
+    if (relevantFrequency(i)) {
+      canvasCtx.fillStyle = "rgb(0,255,0)";
+    } else {
+      canvasCtx.fillStyle = "rgb(255,0,0)";
+    }
     canvasCtx.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight / 2);
 
     x += barWidth + 1;
   }
+
+  const binaryString = getBits(dataArray).join("");
+  const intValue = parseInt(binaryString, 2);
+
+  // console.log(Date.now() - lastChange);
+  if (lastIntValue === intValue) {
+    if (!recordedCharacter && Date.now() - lastChange > timeThreshold) {
+      recordCharacter({ binaryString, intValue });
+      recordedCharacter = true;
+    }
+  } else {
+    lastChange = Date.now();
+    lastIntValue = intValue;
+    recordedCharacter = false;
+  }
+}
+
+const amplitudeThreshold = 150;
+
+const bitRanges = [
+  [47, 50],
+  [54, 57],
+  [61, 64],
+  [69, 71],
+  [76, 78],
+  [83, 85],
+  [89, 92],
+  [96, 99]
+];
+
+const bitFrequencies = bitRanges.map(range =>
+  Math.round((range[0] + range[1]) / 2)
+);
+
+function getBits(frequencyArray) {
+  return bitFrequencies
+    .map(frequency => (frequencyArray[frequency] > amplitudeThreshold) + 0)
+    .reverse();
+}
+
+function relevantFrequency(index) {
+  return bitRanges.some(([low, high]) => index >= low && index <= high);
+}
+
+function recordCharacter({ binaryString, intValue }) {
+  const character = String.fromCharCode(intValue);
+  numbersView.innerHTML = `${binaryString} -- ${intValue} -- ${character}`;
+
+  historyView.innerHTML = historyView.innerHTML + character;
 }
 
 draw();
