e60ac12d2e7ed7b46679f6b03b2f6a181e24a77e
Nathan Taylor <nbtaylor@gmail.com>
2019-05-03 22:11:12 -0600

    allow [a-z] to send ascii characters

--- index.js
+++ index.js
@@ -95,29 +95,62 @@ var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 var oscillators = new Array(8);
 
 document.body.addEventListener("keydown", event => {
-  if (event.key >= "1" && event.key <= "8" && !event.repeat) {
+  if (event.repeat) {
+    return;
+  }
+  if (event.key >= "1" && event.key <= "8") {
     startPlaying(parseInt(event.key));
+  }
+  else if (event.key.match(/[a-z ]/)) {
+    playbyte(event.key.charCodeAt(0));
   }
 });
 document.body.addEventListener("keyup", event => {
-  if (event.key >= "1" && event.key <= "8" && !event.repeat) {
+  if (event.repeat) {
+    return;
+  }
+  if (event.key >= "1" && event.key <= "8") {
     stopPlaying(parseInt(event.key));
+  }
+  else if (event.key.match(/[a-z ]/)) {
+    resetAll();
   }
 });
 
+function playbyte(b) {
+    console.log("[playbyte] " + b);
+    for (i = 0; i < 8; i++) {
+        if (b & (1 << i)) {
+            startPlaying(i+1);
+        }
+    }
+}
+
+function resetAll() {
+    for (i = 1; i <= 8; i++) {
+        stopPlaying(i);
+    }
+}
+
 function startPlaying(key) {
-  console.log("[start] " + key);
+  //console.log("[start] " + key);
+    if (oscillators[key-1]) {
+        return;
+    }
   var oscillator = audioCtx.createOscillator();
 
   oscillator.type = "sine";
-  oscillator.frequency.value = 900 + (50 * key); // value in hertz
+  oscillator.frequency.value = 901 + (151 * key); // value in hertz
   oscillator.connect(audioCtx.destination);
   oscillator.start();
   oscillators[key-1] = oscillator;
 }
 
 function stopPlaying(key) {
-  console.log("[stop] " + event.key);
+  //console.log("[stop] " + event.key);
+    if (oscillators[key-1] === null || oscillators[key-1] === undefined) {
+        return;
+    }
   oscillators[key-1].stop();
   oscillators[key-1] = null;
 }
